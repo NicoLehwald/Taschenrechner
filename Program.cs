f33f314b0c6bbacb6958c2109644cfe1254c9f51
PanidevNiLe <nile@panidev.de>
2026-08-02 17:00:42 +0200

    Taschenrechner Version 0.1 umgesetzt

--- Program.cs
+++ Program.cs
@@ -4,7 +4,46 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("=================================\r\n        Taschenrechner\r\n=================================");
+           
+            Console.WriteLine("Bitte geben Sie die erste Zahl ein.");
+            string ersteEingabe = Console.ReadLine();
+
+            double ersteZahl = Convert.ToDouble(ersteEingabe);
+
+            Console.WriteLine("Bitte geben Sie die zweite Zahl ein.");
+            string zweiteEingabe = Console.ReadLine();
+
+            double zweiteZahl = Convert.ToDouble(zweiteEingabe);
+
+            Console.WriteLine("Bitte geben Sie die gewünschte Operation (+,-,*,/) ein.");
+            string operation = Console.ReadLine();
+
+            double ergebnis;
+
+            switch(operation) 
+            {
+                case "+":
+                   ergebnis =  ersteZahl + zweiteZahl;
+                    break;
+                case "-":
+                    ergebnis = ersteZahl - zweiteZahl;
+                    break;
+                case "*":
+                    ergebnis = ersteZahl * zweiteZahl;
+                    break;
+                case "/":
+                    ergebnis = ersteZahl / zweiteZahl;
+                    break;
+                default:
+                    Console.WriteLine("Die Eingabe ist ungültig. Es wurde eine falsche Operation ausgewählt. Bitte wiederholen Sie den Vorgang erneut.");
+                    ergebnis = 0;
+                    break;
+            }
+
+            Console.WriteLine("Das Ergebnis der Operation lautet: " + ergebnis);
+
         }
     }
 }
